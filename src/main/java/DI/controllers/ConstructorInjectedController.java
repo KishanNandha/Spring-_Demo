7cdd9e878ba07edc6aa0e009df7480963ef173eb
KishanNandha <kishan.nandha99@gmail.com>
2019-01-19 21:01:43 +0530

    Added files for @Qualifier

--- src/main/java/DI/controllers/ConstructorInjectedController.java
+++ src/main/java/DI/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package DI.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import DI.services.GreetingService;
@@ -10,9 +11,9 @@ import DI.services.GreetingService;
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
-
+    //@Qualifier is used to specify bean required
     @Autowired
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
